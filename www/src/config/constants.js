b9e1d44335a4b5d8e3b2b7f100f7952a8265a565
WalletAdamants <111858023+WalletAdamants@users.noreply.github.com>
2022-08-24 09:16:50 +0300

    Update constants.js

--- www/src/config/constants.js
+++ www/src/config/constants.js
@@ -1,6 +1,4 @@
 const TARGET_RSS_URL = 'https://lifehacker.com/rss';
 const SALT_COUNT = 10;
-const ACCESS_EXPIRES_IN = '1h';
-const REFRESH_EXPIRES_IN = '30d';
 
-module.exports = { TARGET_RSS_URL, SALT_COUNT, ACCESS_EXPIRES_IN, REFRESH_EXPIRES_IN };
+module.exports = { TARGET_RSS_URL, SALT_COUNT };
